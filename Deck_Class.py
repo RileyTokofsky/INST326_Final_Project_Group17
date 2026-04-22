8d9da9ab5a7c1e6c974713abb2151c628e55a86f
Riley Tokofsky <rtoko@terpmail.umd.edu>
2026-04-22 15:02:19 -0400

    deck class start

--- Deck_Class.py
+++ Deck_Class.py
@@ -1,2 +1,8 @@
-class deck:
-    def __init__
+class Deck:
+    
+    def __init__(self, num_decks: int = 1):
+        self.num_decks = num_decks
+        self.cards = []
+        self.percent_of_deck_used = 0.0
+        self.build_deck()
+        self.shuffle()
