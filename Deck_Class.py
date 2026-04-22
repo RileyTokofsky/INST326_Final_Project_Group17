d815d4872eaa34f0981dace1ae42b421f16e3cbb
Riley Tokofsky <rtoko@terpmail.umd.edu>
2026-04-22 15:03:47 -0400

    def build_deck done

--- Deck_Class.py
+++ Deck_Class.py
@@ -6,3 +6,13 @@ class Deck:
         self.percent_of_deck_used = 0.0
         self.build_deck()
         self.shuffle()
+    
+    def build_deck(self):
+        ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
+                 "Jack", "Queen", "King", "Ace"]
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        
+        for deck_num in range(self.num_decks):
+            for suit in suits:
+                for rank in ranks:
+                    self.cards.append(Card(rank, suit))
