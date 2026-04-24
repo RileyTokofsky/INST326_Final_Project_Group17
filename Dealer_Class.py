d89d7fee9993222d1300e870985355df61be3dcf
spandey8-maker <spandey8@terpmail.umd.edu>
2026-04-24 10:51:47 -0400

    Update Dealer_Class.py

--- Dealer_Class.py
+++ Dealer_Class.py
@@ -1,8 +1,39 @@
 class Dealer:
-
-deck = [2,3,4,5,6,7,8,9,10,11,12,13,14]
-
-def deal (deal):
-hand = []
+    
+    def __init__(self):
+        self.hand = []
+    
+    def deal_to_self(self, deck):
+        card = deck.deal_card()
+        self.hand.append(card)
+    
+    def show_hand(self, reveal_all=False):
+        if reveal_all:
+            return [(card.rank, card.suit) for card in self.hand]
+        else:
+            if len(self.hand) > 0:
+                return [("Hidden", ""), (self.hand[1].rank, self.hand[1].suit)]
+            return []
+    
+    def get_hand_value(self):
+        total = 0
+        aces = 0
+        
+        for card in self.hand:
+            total += card.get_value()
+            if card.rank.lower() == "ace":
+                aces += 1
+        
+        while total > 21 and aces > 0:
+            total -= 10
+            aces -= 1
+        return total
+    
+    def play_turn(self, deck):
+        while self.get_hand_value() < 17:
+            self.deal_to_self(deck)
+    
+    def reset_hand(self):
+        self.hand = []
 
 
