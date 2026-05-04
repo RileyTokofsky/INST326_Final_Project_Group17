38605cfb8e4df3705a1922c06c3fce72fa7a0680
Michael Ama <mama@terpmail.umd.edu>
2026-05-04 19:18:32 -0400

    outlined system class

--- System.py
+++ System.py
@@ -1,5 +1,12 @@
 #---------------------------WORKFLOW-------------------------------------------
 #Ask how many players are playing using input()
 #Loop up to number of players to create player objects
-#Start loop for game
-#
+#Start loop for game until each player says end when asked to bet
+#everyone must bet
+#   Ask for optional pre bet
+#dealer deals everyone 2 cards
+#dealer deals himself 1 card
+#Dealer asks each player to hit or stand, looping until they say stand
+#dealer deals themself cards until total value >=17
+#Dealer compares their hand to players and decides who wins and side bets
+#Betting loop starts again
