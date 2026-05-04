29560cfcca6433c2a1e618047b65cde81c98f8b5
Michael Ama <mama@terpmail.umd.edu>
2026-05-04 16:26:51 -0400

    added system stuff

---
+++ System.py
@@ -0,0 +1,5 @@
+#---------------------------WORKFLOW-------------------------------------------
+#Ask how many players are playing using input()
+#Loop up to number of players to create player objects
+#Start loop for game
+#
