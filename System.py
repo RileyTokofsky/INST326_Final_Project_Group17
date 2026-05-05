5c872f78fb725a535dcced4c69cc9d19a01453d5
Michael Ama <mama@terpmail.umd.edu>
2026-05-04 21:46:17 -0400

    added part of system implementation

--- System.py
+++ System.py
@@ -1,6 +1,14 @@
 #---------------------------WORKFLOW-------------------------------------------
+def main(self):
 #Ask how many players are playing using input()
+#   error handling later
+    people = input("how many player are playing today?")
+    num = 0
+    players = 
 #Loop up to number of players to create player objects
+    while num < people:
+        response = input("tell us your name and how much money you brought to the table")
+        
 #Start loop for game until each player says end when asked to bet
 #everyone must bet
 #   Ask for optional pre bet
